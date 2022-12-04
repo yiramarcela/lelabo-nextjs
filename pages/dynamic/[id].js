438a47bd50774d40accd1a9bdc9e2a52e52a423b
MichaelRomero96 <michael.romero@meltstudio.co>
2022-12-04 12:14:36 -0500

    dynamic test

--- pages/dynamic/[id].js
+++ pages/dynamic/[id].js
@@ -1,11 +1,9 @@
 import { useRouter } from "next/router";
 
 const DynamicPage = () => {
-  const router = useRouter();
   return (
     <div>
       <h1>Desde ruta dinamica</h1>
-      {router.query.id}
     </div>
   );
 };
